4a7b430e26cf403a3e7444192914d2a95348fce1
Jack541970 <139237298+Jack541970@users.noreply.github.com>
2023-11-24 15:14:44 +0100

    stopped

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import About from './components/About'
+import Search from './components/Search'
+import { View, StatusBar } from 'react-native'
+import { TabNavigator } from 'react-navigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tabs = TabNavigator({
+  Search: { screen: Search },
+  About: { screen: About }
+}, {
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    showIcon: true,
+    showLabel: false,
+    indicatorStyle: {
+      height: 2,
+      backgroundColor: '#FFF'
+    },
+    style: {
+      backgroundColor: "#a2273C",
+      borderTopWidth: 1,
+      borderColor: "#3f101c"
+    }
+  },
+})
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <StatusBar hidden={true}/>
+        <Tabs />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ components/Search.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import style from '../Style'
+import { TextInput, Image, Button, View, Keyboard } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import List from './List'
+
+class Search extends React.Component {
+
+  static navigationOptions = {
+    title: 'Rechercher une ville',
+    tabBarIcon: () => {
+      return <Image source={require('./icons/home.png')} style={{width:20, height: 20}}/>
+    }
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      city: ''
+    }
+  }
+
+  setCity (city) {
+    this.setState({city})
+  }
+
+  submit () {
+    Keyboard.dismiss()
+    this.setState({city: ''})
+    this.props.navigation.navigate('Result', {city: this.state.city})
+  }
+
+  render () {
+    return (
+      <View style={style.container}>
+        <TextInput
+          underlineColorAndroid='transparent'
+          onChangeText={(text) => this.setCity(text)}
+          onSubmitEditing={() => this.submit()}
+          style={style.input}
+          placeholder="Entrez votre ville"
+          value={this.state.city}
+        />
+        <Button color={style.color} onPress={() => this.submit()} title="Rechercher"/>
+      </View>
+    )
+  }
+
+}
+
+const navigationOptions = {
+  headerStyle: style.header,
+  headerTitleStyle: style.headerTitle
+}
+
+export default StackNavigator({
+  Search: {
+    screen: Search,
+    navigationOptions
+  },
+  Result: {
+    screen: List,
+    navigationOptions
+  }
+})
